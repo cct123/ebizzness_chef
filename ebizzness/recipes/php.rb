db1eda0168c4cff8b354896e0e95173918e12371
cct123 <liuyez123@gmail.com>
2020-11-13 10:35:24 -0500

    Update php.rb

--- ebizzness/recipes/php.rb
+++ ebizzness/recipes/php.rb
@@ -4,15 +4,10 @@ execute "amazon-linux-extras" do
   action :run
 end
   
-%w{php php-cli php-common php-fpm php-mbstring php-mysqlnd php-pdo php-pear php-cgi php-xml php-curl php-gettext php-json php-fpm php-intl php-zip}.each do |pkg|
+%w{php php-cli php-common php-fpm php-mbstring php-mysqlnd php-pdo php-pear php-cgi php-xml php-curl php-gettext php-json php-fpm php-intl php-zip imagick}.each do |pkg|
   package pkg do
     action :install
   end
-end
-
-execute "install php-imagick" do
-  command "yum install php-imagick –y"
-  action :run
 end
 
 # Enable start on boot and start Apache
